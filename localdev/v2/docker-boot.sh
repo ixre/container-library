369ce4e1cfd910857afd35cbc424490b3846cddb
jarrysix <jarrysix@gmail.com>
2020-12-20 02:27:08 +0800

    v2

---
+++ localdev/v2/docker-boot.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+#!/usr/bin/env sh
+
+# fix permission
+#chown -R $(id -u):$(id -g) /var/lib/mysql/;chown -R mysql:mysql /var/lib/mysql/
+
+
+# init data
+if [ ! -f "/data/etc" ];then
+ 	mkdir -p /data/etc
+fi
+
+if [ ! -f "/data/etc/redis.conf" ];then
+    cp /etc/redis.conf /data/etc/
+else
+    cat /data/etc/redis.conf > /etc/redis.conf
+fi
+
+if [ ! -f "/data/etc/profile" ];then
+    cp /etc/profile /data/etc/
+else
+    cat /data/etc/profile > /etc/profile
+fi
+
+if [ ! -f "/data/nginx/conf/nginx.conf" ];then
+    mkdir -p /data/nginx/conf && cp /usr/local/nginx/conf/nginx.conf /data/nginx/conf/nginx.conf
+else
+    \cp -rf /data/nginx/conf/*.conf  /usr/local/nginx/conf/
+fi
+
+
+# Start nginx
+/usr/local/nginx/sbin/nginx
+echo "[ Local-Dev][ OK]: Nginx started successfully!"
+
+
+# Start redis-server
+echo "[ Local-Dev][ OK]: Redis started successfully!"
+redis-server /etc/redis.conf 
+
---
+++ localdev/v2/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker rm -f localdev
+docker run -ti --rm --name localdev \
+    --add-host master:172.17.0.1 \
+    --volume $(pwd)/tmp/data:/data \
+	--volume $(pwd)/tmp/mysql:/var/lib/mysql \
+    jarry6/localdev:latest sh
+
