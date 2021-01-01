ce5f6be8b60d9fca7ae860559da52225cb00372e
jarrysix <jarrysix@gmail.com>
2021-01-01 23:31:55 +0800

    localdev

--- localdev/docker-boot.sh
+++ localdev/docker-boot.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-#!/usr/bin/env sh
 
 # fix permission
 #chown -R $(id -u):$(id -g) /var/lib/mysql/;chown -R mysql:mysql /var/lib/mysql/
 
+set -e
 
 # init data
 if [ ! -f "/data/etc" ];then
