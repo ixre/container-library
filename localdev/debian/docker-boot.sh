90d13e25f40d4757a7743de10b1c23f81e3c6b66
jarrysix <jarrysix@gmail.com>
2021-11-02 17:18:19 +0800

    Update docker-boot.sh

--- localdev/debian/docker-boot.sh
+++ localdev/debian/docker-boot.sh
@@ -10,11 +10,6 @@ if [ ! -f "/data/etc" ];then
  	mkdir -p /data/etc
 fi
 
-if [ ! -f "/data/etc/redis.conf" ];then
-    cp /etc/redis.conf /data/etc/
-else
-    cat /data/etc/redis.conf > /etc/redis.conf
-fi
 
 if [ ! -f "/data/etc/profile" ];then
     cp /etc/profile /data/etc/
@@ -28,60 +23,68 @@ else
     \cp -rf /data/nginx/conf/*.conf  /usr/local/nginx/conf/
 fi
 
-# 初始化Mariadb Server
-if [ ! -f "/data/etc/mariadb.cnf" ];then
-	cp /etc/mysql/mariadb.conf.d/50-server.cnf /data/etc/mariadb.cnf
-else
-    \cp -rf /data/etc/mariadb.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
-fi
-
-# 如果/run/mysql目录不存在,则创建并设置权限
-if [ ! -d "/run/mysqld" ]; then
-	echo "[ Local-Dev][ Mariadb]:creating /run/mysqld "
-	mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld &&\
-	mkdir -p /etc/mysql/conf.d
-fi
-# 如果mysql目录不存在，则初始化数据库
-if [ ! -d "/var/lib/mysql/mysql" ];then
-	# mysql8
-	#&& mysql -e 'CREATE USER "root"@"%" IDENTIFIED by '${MYSQL_ROOT_PASSWORD}'"'\
-	#	'; GRANT ALL ON *.* to "root"@"%" with grant option;' \
-	#&& mysql -e 'CREATE USER "root"@"172.17.%" IDENTIFIED by ""' \
-	#	'; GRANT ALL ON *.* to "root"@"172.17.%" with grant option;' \
-
-    chown -R mysql:mysql /var/lib/mysql \
-	&& mysql_install_db --user=mysql --datadir=/var/lib/mysql --ldata=/var/lib/mysql \
-	&& sh -c 'mysqld_safe&' && sleep 5 \
-	&& mysql -e 'GRANT ALL ON *.* TO "root"@"%" identified by "'${MYSQL_ROOT_PASSWORD}'";FLUSH PRIVILEGES;' \
-	&& mysql -e 'CREATE USER "root"@"172.17.%" IDENTIFIED by "";GRANT ALL ON *.* TO "root"@"172.17.%" identified by "";' \
-	&& mysql -e 'FLUSH PRIVILEGES;SELECT user,host from mysql.user;' \
-	&& service mysql stop
-	#&& ps -ef|grep mysqld -m1 | awk '{print $2}'|xargs kill -15
-	if [ $? -eq 0 ]; then 
-		echo "[ Local-Dev][ Mariadb]: Data initialize successfully!"
+if [ "${SERVE_REDIS}" = "true" ];then
+	if [ ! -f "/data/etc/redis.conf" ];then
+	    cp /etc/redis.conf /data/etc/
 	else
-		cat /var/lib/mysql/*.err
-		ps -ef|grep mysqld|awk '{print $2}'|xargs kill -15 # kill mysqld_safe on debian
-		echo "[ Local-Dev][ Mariadb]: Data initialize failed!"
+	    cat /data/etc/redis.conf > /etc/redis.conf
 	fi
-	ps -ef
+	
+	# Start redis-server
+	redis-server /etc/redis.conf &
+	echo "[ Local-Dev][ OK]: Redis started successfully!"
 fi
 
+if [ "${SERVE_MYSQL}" = "true" ];then
+	# 初始化Mariadb Server
+	if [ ! -f "/data/etc/mariadb.cnf" ];then
+		cp /etc/mysql/mariadb.conf.d/50-server.cnf /data/etc/mariadb.cnf
+	else
+	    \cp -rf /data/etc/mariadb.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+	fi
 
-# Start redis-server
-redis-server /etc/redis.conf &
-echo "[ Local-Dev][ OK]: Redis started successfully!"
+	# 如果/run/mysql目录不存在,则创建并设置权限
+	if [ ! -d "/run/mysqld" ]; then
+		echo "[ Local-Dev][ Mariadb]:creating /run/mysqld "
+		mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld &&\
+		mkdir -p /etc/mysql/conf.d
+	fi
+	# 如果mysql目录不存在，则初始化数据库
+	if [ ! -d "/var/lib/mysql/mysql" ];then
+		# mysql8
+		#&& mysql -e 'CREATE USER "root"@"%" IDENTIFIED by '${MYSQL_ROOT_PASSWORD}'"'\
+		#	'; GRANT ALL ON *.* to "root"@"%" with grant option;' \
+		#&& mysql -e 'CREATE USER "root"@"172.17.%" IDENTIFIED by ""' \
+		#	'; GRANT ALL ON *.* to "root"@"172.17.%" with grant option;' \
 
+	    chown -R mysql:mysql /var/lib/mysql \
+		&& mysql_install_db --user=mysql --datadir=/var/lib/mysql --ldata=/var/lib/mysql \
+		&& sh -c 'mysqld_safe&' && sleep 5 \
+		&& mysql -e 'GRANT ALL ON *.* TO "root"@"%" identified by "'${MYSQL_ROOT_PASSWORD}'";FLUSH PRIVILEGES;' \
+		&& mysql -e 'CREATE USER "root"@"172.17.%" IDENTIFIED by "";GRANT ALL ON *.* TO "root"@"172.17.%" identified by "";' \
+		&& mysql -e 'FLUSH PRIVILEGES;SELECT user,host from mysql.user;' \
+		&& service mysql stop
+		#&& ps -ef|grep mysqld -m1 | awk '{print $2}'|xargs kill -15
+		if [ $? -eq 0 ]; then 
+			echo "[ Local-Dev][ Mariadb]: Data initialize successfully!"
+		else
+			cat /var/lib/mysql/*.err
+			ps -ef|grep mysqld|awk '{print $2}'|xargs kill -15 # kill mysqld_safe on debian
+			echo "[ Local-Dev][ Mariadb]: Data initialize failed!"
+		fi
+		ps -ef
+	fi
+	
+	# Start mariadb
+	chown -R mysql:mysql /var/lib/mysql
+	mysqld_safe --user=mysql --datadir=/var/lib/mysql &
+	echo "[ Local-Dev][ OK]: Mariadb started successfully!"
+fi
 
 # Start nginx
-/usr/local/nginx/sbin/nginx
+/usr/local/nginx/sbin/nginx -g "daemon off;"
 echo "[ Local-Dev][ OK]: Nginx started successfully!"
 
-
-# Start mariadb
-chown -R mysql:mysql /var/lib/mysql
-mysqld_safe --user=mysql --datadir=/var/lib/mysql
-echo "[ Local-Dev][ OK]: Mariadb started successfully!"
 
 
 
